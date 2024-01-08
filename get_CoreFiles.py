51b327f9d6476e6386e5c4c629e08574d02959a1
elral <3263285+elral@users.noreply.github.com>
2024-01-08 16:03:10 +0100

    delete platformio.ini in /src if it was cloned

--- get_CoreFiles.py
+++ get_CoreFiles.py
@@ -10,6 +10,8 @@ if not os.path.exists(CORESOURCE_DIR):
     print ("Cloning Mobiflight-CustomDevices repo ... ")
     env.Execute(f"git clone --depth 1 --filter=blob:none --sparse --branch {CORESOURCE_TAG} https://github.com/MobiFlight/MobiFlight-FirmwareSource $PROJECT_DIR/src")
     env.Execute(f"git --work-tree=$PROJECT_DIR/src --git-dir=$PROJECT_DIR/src/.git sparse-checkout set _Boards src")
+    if os.path.isfile(CORESOURCE_DIR + "/platformio.ini"):
+        os.remove(CORESOURCE_DIR + "/platformio.ini")
 else:
     print ("Checking for Mobiflight-CustomDevices repo updates ... ")
     env.Execute(f"git --work-tree=$PROJECT_DIR/src --git-dir=$PROJECT_DIR/src/.git pull origin {CORESOURCE_TAG} --depth 100")
